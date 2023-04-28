eedea2e89410f41fadab1532e288fca65946999a
Top29411 <hhrdoltan29@gmail.com>
2023-04-28 20:12:37 +0900

    updated evm-provider

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  root: './src',
   build: {
-    outDir: '../dist',
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "util-lib",
+      fileName: "util-lib",
+    },
   },
-  server: {
-    port: 3000,
-  },
+  plugins: [dts()],
 });
